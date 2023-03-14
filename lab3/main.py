c77d27318ccf9e2ab3d26eef5172c9bb0aeda956
Nistiah <56507634+Nistiah@users.noreply.github.com>
2023-03-14 17:24:26 +0100

    update main

--- lab3/main.py
+++ lab3/main.py
@@ -1,23 +1,40 @@
 #!/usr/bin/env python3
 
+
 import sys
+import re
 
 def my_printf(format_string,param):
-    length = len(format_string)
-    i = 0
-    while(i<length):
-    	if(format_string[i] == '#' and format_string[i+1]=='.' and format_string[i+2].isdigit and format_string[i+3]=='k'):
-    	    lenght = int(format_string[i+2])
-    	    param = param.swapcase()
-    	    ##param=param.rjust(lenght, ' ')
-    	    print(param[0:int(format_string[i+2])], end="")
-    	    i=i+4
-    	elif (format_string[i] == '#' and format_string[i+1] == 'k'):
-    	    print(param.swapcase(), end="")
-    	    i=i+2
-    	else:
-    	    print(format_string[i], end="")
-    	    i=i+1
+    REGEX = r'#(\d+)?(\.)?(\d+)?k '
+    shouldDo=True
+    param = param.swapcase()
+    for idx in range(0,len(format_string)):
+        if shouldDo:
+            if format_string[idx] == '#':
+                result = re.search(REGEX, format_string[idx:])
+                firstdigit = result.group(1)
+                dot = result.group(2)
+                seconddigit = result.group(3)
+                if not firstdigit and not seconddigit:
+                    print(param,end="")
+                elif firstdigit and seconddigit and dot:
+                    firstdigitInt = int(firstdigit)
+                    seconddigitInt = int(seconddigit)
+                    print(f'{param:>{firstdigitInt}.{seconddigitInt}}',end="")
+                elif firstdigit and not seconddigit and not dot:
+                    firstdigitInt = int(firstdigit)
+                    print(f'{param:>{firstdigitInt}}',end="")
+                elif not firstdigit and seconddigit and dot:
+                    seconddigitInt = int(seconddigit)
+                    print(f'{param:.{seconddigitInt}}',end="")
+                else:
+                    break
+                shouldDo=False
+            else:
+                print(format_string[idx],end="")
+        else:
+            if format_string[idx] == 'k':
+                shouldDo=True
     print("")
 
 data=sys.stdin.readlines()
