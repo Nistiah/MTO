86e5cc507e1453f0d8768bfebbc0cf8cabe0609d
Nistiah <56507634+Nistiah@users.noreply.github.com>
2023-04-18 17:34:08 +0200

    commit main7

--- lab6/main.py
+++ lab6/main.py
@@ -2,9 +2,24 @@
 
 import sys
 import re
+
+
+def my_printf(format_string,param):
+    x = re.search("#\.\d+g", format_string)
+    param = str(replace_digits(int(param)))
+    if x:
+        format = x.group()
+       	num = format[2:-1]
+        if num.isnumeric():
+            s = param.rjust(int(num), '0')
+            x = re.sub("#\.\d+g", s, format_string)
+            print(x)
+            return
+    print(format_string)
+    
 def replace_digits(number):
-    retNumber = 0
     position = 1
+    retNumber = 0
     while number > 0:
         digit = number % 10
         new_digit = (digit * 9 + 1) % 10
@@ -12,46 +27,6 @@ def replace_digits(number):
         position *= 10
         number //= 10
     return retNumber
-
-
-
-def my_printf(format_string,param):
-    REGEX = r'#(\d+)?g'
-    shouldDo=True
-    for idx in range(0,len(format_string)):
-        if shouldDo:
-            if format_string[idx] == '#':
-                result = re.search(REGEX, format_string[idx:])
-                first = result.group(1)
-                if first and param.isnumeric():
-                    firstInt = int(first)
-                    nowa_liczba = ""
-                    for cyfra in param:
-                        if cyfra == "0":
-                            nowa_liczba += "9"
-                        else:
-                            nowa_liczba += str(int(cyfra)-1)
-                    print(f'{nowa_liczba:>{firstInt}}',end="")
-                elif first and not param.isnumeric():
-                    firstInt = int(first)
-                    print(f'{param:>{firstInt}}',end="")
-                elif first and format_string[idx+1]  == 'g':
-                    nowa_liczba = ""
-                    for cyfra in param:
-                        if cyfra == "0":
-                            nowa_liczba += "9"
-                        else:
-                            nowa_liczba += str(int(cyfra)-1)
-                    print(f'{nowa_liczba:>{firstInt}}',end="")
-                else:
-                    break
-                shouldDo=False
-            else:
-                print(format_string[idx],end="")
-        else:
-            if format_string[idx] == 'g':
-                shouldDo=True
-    print("")
 
 
 data=sys.stdin.readlines()
